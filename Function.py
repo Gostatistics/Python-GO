a7d98d01429c0e198845e0b16e9986b04c6dbf26
Gostat <46393584+Gostatistics@users.noreply.github.com>
2020-02-03 13:02:19 -0800

    Update Function.py

--- Function.py
+++ Function.py
@@ -10,27 +10,24 @@ def isnull(x):
   output = False
   if pd.isnull(x):
     output = True
-  elif isinstance(x, str):
-    if x.lower()=='null':
-      outout = True
-    if x.lower()=='nan':
-      output = True
-    if x.lower()=='na':
-      output = True
+  elif isinstance(x, str) and (x.lower() in ['null', 'nan', 'na']):
+    outout = True
   else:
-      output = False
+    output = False
   return output
- 
+
 ### Output the missing value count for all columns.
 from pyspark.sql import DataFrame
 def null_report(df):
-  dfp = df
-  if isinistance(df, DataFrame): dfp = df.toPandas()
+  dfp = df.toPandas()
+  dl = []
   cols = dfp.columns
-  nrow = dfp.shape[0]
-  print('Total entry count: ',nrow)
-  for i in cols:
-    print('Missing value count for ', i, ': ',  dfp[i].apply(lambda l: isnull(l)).sum())
+  dl.append(['Total Entry', dfp.shape[0]])
+  for i in range(len(cols)):
+    dl.append([cols[i], dfp[cols[i]].apply(lambda l: isnull(l)).sum()])
+  dl = pd.DataFrame(dl)
+  dl.columns = ['Column', 'Null Count']
+  return dl
 
 ### Reorder strings in the format to be used as keys
 def sorted_title(x):
